eb36bdea0f2cec8200c324e0251c1849a7b228a8
ebtrinh <99101731+ebtrinh@users.noreply.github.com>
2025-08-10 13:01:09 -0400

    fixdep5

--- code/musicbuddy/src/lib/supabaseClient.ts
+++ code/musicbuddy/src/lib/supabaseClient.ts
@@ -2,4 +2,4 @@
 import { createClient } from '@supabase/supabase-js';
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
-export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
+export const supabase = createClient("https://lcpdqaacsdldmxjbavkj.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxjcGRxYWFjc2RsZG14amJhdmtqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ3OTA1MzYsImV4cCI6MjA3MDM2NjUzNn0.4XeNR0gABwzniL_9JdEThLndPRlxxeQ5EInLhLyzahk");
